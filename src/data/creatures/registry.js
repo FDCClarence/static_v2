ecc41a0106e52e4603802a6a758365bb84991a3c
FDCClarence <fdc.clarenced@gmail.com>
2026-04-13 15:05:27 +0800

    balance stalker

--- src/data/creatures/registry.js
+++ src/data/creatures/registry.js
@@ -2,14 +2,14 @@
 export default [
   {
     id: 'stalker',
-    volume: 0.35,
+    volume: 0.5,
     speed: 3,
     hearingRadius: 6,
     behavior: 'stalk',
     devLabel: 'STK',
     sounds: {
       ambient_sound: 'stalk-breathing.wav',
-      ambient_timer: 3,
+      ambient_timer: 12,
       move_sound: 'zombie-gasp.wav',
     },
   },
